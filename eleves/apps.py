05991234a5a2c5e80900808700bddebeb9d49b1c
Faraleno2022 <116464205+Faraleno2022@users.noreply.github.com>
2025-10-23 11:52:29 +0000

    signals

--- eleves/apps.py
+++ eleves/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ElevesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'eleves'
+    
+    def ready(self):
+        """Importer les signals lors du démarrage de l'application"""
+        import eleves.signals
