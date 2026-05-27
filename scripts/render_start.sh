4f6a0e28c78210d2884dbcebe12a4bcc89978310
Godecode62 <godecode62@gmail.com>
2026-05-27 07:34:18 +0000

    Create default school on Render startup

--- scripts/render_start.sh
+++ scripts/render_start.sh
@@ -2,6 +2,7 @@
 set -e
 
 python manage.py migrate --noinput
+python manage.py ensure_default_school
 python manage.py ensure_render_admin
 python manage.py collectstatic --noinput
 gunicorn ecole_moderne.wsgi:application
