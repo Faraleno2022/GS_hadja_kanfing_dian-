7f46ff7a27161b815fcdb3e2af0b033dc749d118
Godecode62 <godecode62@gmail.com>
2026-06-03 15:31:36 +0000

    Charger env PythonAnywhere pour migrations

--- ecole_moderne/settings.py
+++ ecole_moderne/settings.py
@@ -239,11 +239,11 @@ else:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.mysql",
-            "NAME": "myschoolgn$myschooldb",
-            "USER": "myschoolgn",
-            "PASSWORD": "Felixsuzaneleno1994@",
-            "HOST": "myschoolgn.mysql.pythonanywhere-services.com",
-            "PORT": "3306",
+            "NAME": os.environ.get("DJANGO_DB_NAME", "myschoolgn$myschooldb"),
+            "USER": os.environ.get("DJANGO_DB_USER", "myschoolgn"),
+            "PASSWORD": os.environ.get("DJANGO_DB_PASSWORD", ""),
+            "HOST": os.environ.get("DJANGO_DB_HOST", "myschoolgn.mysql.pythonanywhere-services.com"),
+            "PORT": os.environ.get("DJANGO_DB_PORT", "3306"),
             "OPTIONS": {
                 "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
                 "charset": "utf8mb4",
